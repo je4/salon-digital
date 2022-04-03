e6364b98ae9b61062073cdabed1c8dbd37845aed
Jürgen Enge <github@betalabs.ch>
2022-04-03 14:13:56 +0200

    content moved to pkg

---
+++ pkg/content/content.go
@@ -0,0 +1,21 @@
+package content
+
+type Person struct {
+	Name string `json:"name"`
+	Role string `json:"role"`
+}
+
+type Media struct {
+	Type      string
+	Thumbnail string
+	Media     string
+}
+
+type Content struct {
+	Title    string           `json:"title"`
+	Subtitle string           `json:"subtitle"`
+	Persons  []Person         `json:"persons"`
+	Year     string           `json:"year"`
+	Poster   string           `json:"poster"`
+	Medias   map[string]Media `json:"medias"`
+}
